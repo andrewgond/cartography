4de0e08c99a67d2e524e78e4ec98f284d5ec6fef
okaycoffee <onebishnu@gmail.com>
2022-07-29 14:59:41 -0700

    added short shuffle script

---
+++ scripts/tsv_shuffle.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: tsv_shuffle INPUT_TSV OUTPUT_TSV")
+    exit()
+
+print("Importing...")
+full_train = pd.read_csv(sys.argv[1], sep='\t', header=0)
+print("Imported! Now shuffling...")
+shuffled = full_train.sample(frac=1)
+print("Shuffled! Now saving...")
+shuffled.to_csv(sys.argv[2], sep="\t", index=False)
+print("Saved!")
